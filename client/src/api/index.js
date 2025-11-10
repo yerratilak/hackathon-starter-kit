1c15a69446252a4822287e8b58ba413dfd143b34
yerratilak <tilakyerra@gmail.com>
2025-11-10 19:34:56 +0400

    updated config

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:1411'
+const API_BASE_URL = 'https://hackathon-starter-kit.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
