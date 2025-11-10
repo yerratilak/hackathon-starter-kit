ffcc973e10fac7f52614710cb232093e104e7e84
yerratilak <tilakyerra@gmail.com>
2025-11-10 19:03:14 +0400

    vit config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: "/client/",
+  server: {
+    allowedHosts: ['hackathon-starter-kit.onrender.com']
+  }	
 })
